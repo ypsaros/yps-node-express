1b005be981d4acee6cfb04a57f946d1f9ae1c145
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2023-09-04 09:58:18 +0300

    Start of express module

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000....')
+})
