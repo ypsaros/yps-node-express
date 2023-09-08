ec0e9dd73bb72cd7e53ab7c2933db30079288064
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2023-09-08 09:16:17 +0300

    Routes setup

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const port = 3000
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task manager app')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}....`)
+})
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,26 @@
+const getAllTasks = (req, res) => {
+    res.send('Get all tasks')
+}
+
+const createTask = (req, res) => {
+    res.json(req.body)
+}
+
+const getTask = (req, res) => {
+    res.json({ id: req.params.id })
+}
+
+const updateTask = (req, res) => {
+    res.send('Update a task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('Delete a task')
+}
+
+
+
+
+module.exports = {
+    getAllTasks, createTask, getTask, updateTask, deleteTask
+}
