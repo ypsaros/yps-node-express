ff61d2d6574fe61f25cca2f4dec4750b72352b6e
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2023-09-14 08:53:05 +0300

    Adding proper schema and try catch for invalid body

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,9 +1,21 @@
 const Task = require('../models/task')
-const getAllTasks = (req, res) => {
-    res.send('Get all tasks')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({ msg: 'Something went wrong, please try again' })
+    }
 }
-const createTask = (req, res) => {
-    res.json(req.body)
+
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: 'Something went wrong, please try again' })
+    }
 }
 const getTask = (req, res) => {
     res.json({ id: req.params.id })
