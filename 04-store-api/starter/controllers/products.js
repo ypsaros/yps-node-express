92fca924e62be274ea3991690c10848729bc1b23
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2023-09-21 15:48:41 +0300

    Populate db

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,12 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('Testing async errors');
-    res.status(200).json({ msg: 'Products testing route' });
+    const products = await Product.find()
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'Products route' });
+    res.status(200).json({ msg: 'Products route' })
 }
 
 module.exports = {
